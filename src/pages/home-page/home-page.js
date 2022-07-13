c93ac4c39699f6aeac66295171e7220ea6523e2e
ibulkin <ibulkin@gmail.com>
2022-07-13 18:05:57 +0300

    4

--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -8,7 +8,7 @@ const GAMES = [
 		category: 'movie',
 		title: 'Pulp Fiction',
 		year: '1994',
-		genres: ['Thriller', 'Crime'],
+		genres: ['Thriller |', 'Crime'],
 		description:
 			"A burger-loving hit man, his philosophical partner, a drug-addled gangster's moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.",
 		image: '/game-covers/pul_fiction.jpg',
@@ -17,12 +17,12 @@ const GAMES = [
 	{
 		id: '002',
 		category: 'movie',
-		title: 'Once Upon a Time in America',
+		title: 'Forrest Gump',
 		year: '1984',
-		genres: ['Drama', 'Crime'],
+		genres: ['Comedy |', 'Drama |', 'Romance'],
 		description:
 			'A former Prohibition-era Jewish gangster returns to the Lower East Side of Manhattan over thirty years later, where he once again must confront the ghosts and regrets of his old life',
-		image: '/game-covers/once_upon_a_time_in_america.jpg',
+		image: '/game-covers/forrest_gump.jpg',
 		video: 'https://www.youtube.com/embed/_gdEuuxW-zk',
 	},
 	{
@@ -30,7 +30,7 @@ const GAMES = [
 		category: 'movie',
 		title: 'Some Like It Hot',
 		year: '1959',
-		genres: ['Comedy', 'Music', 'Romance', 'Crime'],
+		genres: ['Comedy |', 'Music |', 'Romance |', 'Crime'],
 		description:
 			'Two musicians witness a mob hit and struggle to find a way out of the city before they are found by the gangsters. Their only opportunity is to join an all-girl band as they leave on a tour. To make their getaway they must first disguise themselves as women, then keep their identities secret and deal with the problems this brings - such as an attractive bandmate and a very determined suitor.',
 		image: '/game-covers/some_like-it_hot.jpg',
@@ -41,7 +41,7 @@ const GAMES = [
 		category: 'movie',
 		title: 'Reservoir Dogs',
 		year: '1992',
-		genres: ['Thriller', 'Crime'],
+		genres: ['Thriller |', 'Crime'],
 		description:
 			'A botched robbery indicates a police informant, and the pressure mounts in the aftermath at a warehouse. Crime begets violence as the survivors -- veteran Mr. White, newcomer Mr. Orange, psychopathic parolee Mr. Blonde, bickering weasel Mr. Pink and Nice Guy Eddie - unravel.',
 		image: '/game-covers/reservoir_dogs.jpg',
@@ -52,7 +52,7 @@ const GAMES = [
 		category: 'movie',
 		title: 'Inglourious Basterds',
 		year: '1994',
-		genres: ['Drama', 'Action', 'Thriller', 'War'],
+		genres: ['Drama |', 'Action |', 'Thriller |', 'War'],
 		description:
 			'In Nazi-occupied France during World War II, a group of Jewish-American soldiers known as The Basterds are chosen specifically to spread fear throughout the Third Reich by scalping and brutally killing Nazis. The Basterds, lead by Lt. Aldo Raine soon cross paths with a French-Jewish teenage girl who runs a movie theater in Paris which is targeted by the soldiers.',
 		image: '/game-covers/inglourious_basterds.jpg',
@@ -63,7 +63,7 @@ const GAMES = [
 		category: 'movie',
 		title: 'The Godfather',
 		year: '1972',
-		genres: ['Drama', 'Crime'],
+		genres: ['Drama  |', 'Crime'],
 		description:
 			'Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.',
 		image: '/game-covers/the_godfather.jpg',
@@ -72,9 +72,9 @@ const GAMES = [
 	{
 		id: '007',
 		category: 'movie',
-		title: 'Léon: The Professional',
+		title: 'Léon',
 		year: '1994',
-		genres: ['Crime', 'Drama', 'Action'],
+		genres: ['Crime |', 'Drama |', 'Action |'],
 		description:
 			"Léon, the top hit man in New York, has earned a rep as an effective 'cleaner'. But when his next-door neighbors are wiped out by a loose-cannon DEA agent, he becomes the unwilling custodian of 12-year-old Mathilda. Before long, Mathilda's thoughts turn to revenge, and she considers following in Léon's footsteps.",
 		image: '/game-covers/the_professional.jpg',
@@ -85,7 +85,7 @@ const GAMES = [
 		category: 'movie',
 		title: 'GoodFellas',
 		year: '1994',
-		genres: ['Drama', 'Crime'],
+		genres: ['Drama |', 'Crime'],
 		description:
 			'The true story of Henry Hill, a half-Irish, half-Sicilian Brooklyn kid who is adopted by neighbourhood gangsters at an early age and climbs the ranks of a Mafia family under the guidance of Jimmy Conway.',
 		image: '/game-covers/good_fellas.jpg',
